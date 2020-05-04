6e3b666eb57e356ed3327dc508435e01a767f25f
Jetman765 <JTontarski@gmail.com>
2020-05-04 14:57:57 -0400

    exercise 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,7 +5,7 @@ const Statistics = (props) => {
   const {good, neutral, bad} = props;
   const total = good + neutral + bad;
   const average = (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad);
-  const positive = good / (good + neutral + bad);
+  const positive = (good / (good + neutral + bad)) * 100;
 
   if (total === 0) {
     return (
@@ -18,12 +18,17 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={total} />
-      <Statistic text="average" value={average} />
-      <Statistic text="positive" value={positive} />
+
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={total} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positive" value={positive} />
+        </tbody>
+      </table>
     </div>
   );
 }
@@ -31,7 +36,7 @@ const Statistics = (props) => {
 const Statistic = (props) => {
   const {text, value} = props;
 
-  return <div>{text} {value}</div>
+  return (<tr><td>{text}</td><td>{value}</td></tr>)
 }
 
 const App = () => {
