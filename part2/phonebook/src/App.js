c1759e348493951cf36d98314cb6b1d3b5d4d406
Jetman765 <JTontarski@gmail.com>
2020-05-05 11:51:16 -0400

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: 1 }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     persons.filter(person => person.name === newName).length > 0
       ? alert(`${newName} is already added to phonebook`)
@@ -21,6 +23,10 @@ const App = () => {
     setNewName(event.target.value);
   }
 
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -29,11 +35,14 @@ const App = () => {
           name: <input onChange={handleNewName} value={newName} />
         </div>
         <div>
+          number: <input onChange={handleNewNumber} value={newNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} - {person.number}</div>)}
     </div>
   )
 }
